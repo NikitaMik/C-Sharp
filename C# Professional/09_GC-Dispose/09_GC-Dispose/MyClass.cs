47b0e90c09bd9e07069685ceb0eb492e96c8032b
Unknown <hnknta_m@mail.ru>
2018-03-21 23:59:44 +0500

    Garbage Collector

---
+++ C# Professional/09_GC-Dispose/09_GC-Dispose/MyClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _09_GC_Dispose
+{
+    class MyClass : IDisposable
+    {
+
+        #region IDisposable Support
+
+        // Флаг нужен, для отслеживания была ли утилита уже вызвана?
+        private bool disposedValue = false; // Для определения избыточных вызовов
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Освободить управляемое объекты
+                    Console.WriteLine("Соединение с базой данных закрыто.");
+                }
+
+                // TODO: освободить неуправляемые ресурсы (неуправляемые объекты) и переопределить ниже метод завершения.
+                // TODO: задать большим полям значение NULL.
+
+                disposedValue = true;
+            }
+        }
+
+        //Переопределить метод завершения, если необходим.
+         ~MyClass()
+        {
+            // Не изменяйте этот код. Разместите код очистки выше, в методе Dispose(bool disposing).
+            Dispose(false);
+
+            Console.WriteLine("Выполнелся метод - Finalize");
+        }
+
+        public void Dispose()
+        {
+            // Не изменяйте этот код. Разместите код очистки выше, в методе Dispose(bool disposing).
+            Dispose(true);
+
+            // Метод не вызывает деструктор. Нужен если переопределен деструктор.
+            GC.SuppressFinalize(this);  
+        }
+        #endregion
+
+    }
+}
+
---
+++ C# Professional/09_GC-Dispose/09_GC-Dispose/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _09_GC_Dispose
+{
+    class MyClassWrite
+    {
+        ~MyClassWrite()
+        {
+            for (int i = 0; i < 80; i++)
+                Console.Write("|");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClassWrite myw = new MyClassWrite();
+            //Принудительная сборка мусора.
+            GC.Collect();
+            // Garbage Collector может полностью преостанавливать текуший поток.
+            // для проверки, требуется включить оптимизацию  
+            GC.WaitForPendingFinalizers(); 
+
+            for (int i = 0; i < 80; i++)
+                Console.Write(".");
+
+            //
+            Console.WriteLine("\n\nШаблон Microsoft для освобождения ресурсов");
+            MyClass my = new MyClass();
+            for (int i = 0; i < 5; i++)
+                my.Dispose();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+// Разобрать шаблон Microsoft для освобождения ресурсов.
+// Данный паттерн гарантирует, что пользователь, сможет многократно вызывать метод Dispose().
