43100af3ee7453fc0a32de83579beb3cdac82f00
Unknown <hnknta_m@mail.ru>
2018-08-20 20:00:18 +0500

    Singleton

---
+++ Design Pattern/Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+
+            Console.WriteLine(ReferenceEquals(instance1, instance2));
+
+            Console.ReadKey();
+        }
+    }
+}
